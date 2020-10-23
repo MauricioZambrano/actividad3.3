6454eb79206cef919e786cdd15567ed2492468cb
A00827038 <58672371+A00827038@users.noreply.github.com>
2020-10-23 09:25:04 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+#include "BST.h"
+
+
+// Carga los registros del archivo .txt a una BST almacenando objetos de tipo Registro
+// Complejidad: O(n)
+void cargaRegistros(BST &lista){
+    
+}
+
+int main(){
+    BST lista;
+    cargaRegistros(lista);
+    
+    
+    return 0;
+}
